37f29483ae40f330fbb8b0a3ebbf38e8d3b56365
soldholding-hash <soldholding@gmail.com>
2026-07-04 08:41:16 +0100

    tailwind v3 slate colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 export default {
-  content: ["./*.jsx", "./*.html"],
-  theme: { extend: {} },
+  content: ["./App.jsx"],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
+          950: '#020617',
+        }
+      }
+    }
+  },
   plugins: [],
 }
