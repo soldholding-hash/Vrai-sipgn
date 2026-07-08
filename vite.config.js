f989c487b7e69c30da448f387205133ee8cc3d69
soldholding-hash <soldholding@gmail.com>
2026-07-08 23:18:17 +0100

    restructuration: ERP dans /app, vitrine en accueil

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: '.',
+  base: '/app/',
   build: {
-    outDir: 'dist',
+    outDir: 'dist/app',
     chunkSizeWarningLimit: 2000
   }
 })
